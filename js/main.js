b560ca63b0844000e548e434fb4090e0b1a61f99
brunalmucelli <brunalmucelli@gmail.com>
2023-03-01 17:25:46 -0300

    finalização do projeto

--- js/main.js
+++ js/main.js
@@ -1,18 +1,68 @@
 const controle = document.querySelectorAll("[data-controle]");
 
-controle.forEach((elemento)=>{
-    elemento.addEventListener("click",(evento)=>{
+const estatisticas = document.querySelectorAll("[data-estatistica]");
+console.log(estatisticas);
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
-        console.log(evento.target.parentNode)
+        atualizaEstatisticas(evento.target.dataset.controle, evento.target.dataset.peca);
     })
 })
 
-function manipulaDados(operacao, controle){
+function manipulaDados(operacao, controle) {
     const peca = controle.querySelector("[data-contador]");
-    if(operacao==="-"){
-        peca.value=parseInt(peca.value) - 1;
+    if (operacao === "-") {
+        peca.value = parseInt(peca.value) - 1;
     }
-    if(operacao==="+"){
-        peca.value=parseInt(peca.value) + 1;
+    if (operacao === "+") {
+        peca.value = parseInt(peca.value) + 1;
     }
+}
+
+function atualizaEstatisticas(operacao, peca) {
+    estatisticas.forEach((elemento) => {
+        if (operacao === "-") {
+            elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatistica];
+
+        }
+        if (operacao === "+") {
+            elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
+
+        }
+    })
 }
